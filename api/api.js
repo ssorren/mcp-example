31f04e10e2a4433f87152abc73aac40379e40521
Sal Sorrentino <salvatore.sorrentino@konghq.com>
2025-12-04 16:58:57 -0600

    API update

--- api/api.js
+++ api/api.js
@@ -209,6 +209,6 @@ app.get('/openapi', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
